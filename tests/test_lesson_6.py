38fe615fcdf2f1a609c054b2241b67784d7448d8
vershov <v.ershov@ispsystem.com>
2024-11-24 23:36:07 +0800

    1. Добавил class TestLesson6

--- tests/test_lesson_6.py
+++ tests/test_lesson_6.py
@@ -24,38 +24,38 @@ from tests.data.shop_selectors import (
     BUTTON_CREATE_ACCOUNT
 )
 
+class TestLesson6:
+    def test_create_user(self, chrome_driver):
+        driver = chrome_driver
+        driver.get(URL_SHOP)
+        password = str(int(time.time()))
+        email = f'test@{password}test.com'
 
-def test_create_user(chrome_driver):
-    driver = chrome_driver
-    driver.get(URL_SHOP)
-    password = str(int(time.time()))
-    email = f'test@{password}test.com'
+        # Переход к форме создания пользователя
+        driver.find_element(*LINK_USER_CREATE).click()
 
-    # Переход к форме создания пользователя
-    driver.find_element(*LINK_USER_CREATE).click()
+        # Заполнение формы создания пользователя
+        driver.find_element(*INPUT_CREATE_FIRST_NAME).send_keys('test_first_name')
+        driver.find_element(*INPUT_CREATE_LAST_NAME).send_keys('test_last_name')
+        driver.find_element(*INPUT_CREATE_ADDRESS_1).send_keys('test_address_1')
+        driver.find_element(*INPUT_CREATE_POST_CODE).send_keys('12345')
+        driver.find_element(*INPUT_CREATE_CITY).send_keys('test_city')
+        driver.find_element(*INPUT_CREATE_PHONE).send_keys('+11234567890')
+        Select(driver.find_element(*SELECT_CREATE_COUNTRY)).select_by_value('US')
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(SELECT_CREATE_STATE))
+        Select(driver.find_element(*SELECT_CREATE_STATE)).select_by_value('AK')
+        driver.find_element(*INPUT_CREATE_EMAIL).send_keys(email)
+        driver.find_element(*INPUT_CREATE_PASSWORD).send_keys(password)
+        driver.find_element(*INPUT_CREATE_CONFIRM_PASSWORD).send_keys(password)
+        driver.find_element(*BUTTON_CREATE_ACCOUNT).click()
 
-    # Заполнение формы создания пользователя
-    driver.find_element(*INPUT_CREATE_FIRST_NAME).send_keys('test_first_name')
-    driver.find_element(*INPUT_CREATE_LAST_NAME).send_keys('test_last_name')
-    driver.find_element(*INPUT_CREATE_ADDRESS_1).send_keys('test_address_1')
-    driver.find_element(*INPUT_CREATE_POST_CODE).send_keys('12345')
-    driver.find_element(*INPUT_CREATE_CITY).send_keys('test_city')
-    driver.find_element(*INPUT_CREATE_PHONE).send_keys('+11234567890')
-    Select(driver.find_element(*SELECT_CREATE_COUNTRY)).select_by_value('US')
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(SELECT_CREATE_STATE))
-    Select(driver.find_element(*SELECT_CREATE_STATE)).select_by_value('AK')
-    driver.find_element(*INPUT_CREATE_EMAIL).send_keys(email)
-    driver.find_element(*INPUT_CREATE_PASSWORD).send_keys(password)
-    driver.find_element(*INPUT_CREATE_CONFIRM_PASSWORD).send_keys(password)
-    driver.find_element(*BUTTON_CREATE_ACCOUNT).click()
+        # Разлогинивание пользователя
+        driver.find_element(*LINK_USER_LOGOUT).click()
 
-    # Разлогинивание пользователя
-    driver.find_element(*LINK_USER_LOGOUT).click()
+        # Авторизация созданного пользователя
+        driver.find_element(*INPUT_USER_EMAIL).send_keys(email)
+        driver.find_element(*INPUT_USER_PASSWORD).send_keys(password)
+        driver.find_element(*BUTTON_USER_LOGIN).click()
 
-    # Авторизация созданного пользователя
-    driver.find_element(*INPUT_USER_EMAIL).send_keys(email)
-    driver.find_element(*INPUT_USER_PASSWORD).send_keys(password)
-    driver.find_element(*BUTTON_USER_LOGIN).click()
-
-    # Повторное разлогинивание пользователя
-    driver.find_element(*LINK_USER_LOGOUT).click()
+        # Повторное разлогинивание пользователя
+        driver.find_element(*LINK_USER_LOGOUT).click()
