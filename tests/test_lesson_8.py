c5eab7520bdb0df0e4490a763d42c86456ad29e2
vershov <v.ershov@ispsystem.com>
2024-11-27 21:54:26 +0800

    1. Добавил проверку открытия новых окон на всех браузера

--- tests/test_lesson_8.py
+++ tests/test_lesson_8.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -7,12 +8,18 @@ from tests.data.settings import COUNTRIES_PAGE
 
 
 class TestLesson8:
-    def test_open_new_windows(self, chrome_driver, admin_login):
-        driver = chrome_driver
+    @pytest.mark.parametrize(
+        'driver', ['chrome_driver', 'firefox_driver', 'safari_driver'],
+        ids=['driver_chrome', 'driver_firefox', 'safari_driver']
+    )
+    def test_open_new_windows(self,request, driver, custom_admin_login):
+
+        driver = request.getfixturevalue(driver)
+        custom_admin_login(driver)
         driver.get(COUNTRIES_PAGE)
         wait = WebDriverWait(driver, 10)
 
-        driver.find_element(*BUTTON_ADD_NEW_COUNTRY).click()
+        wait.until(EC.visibility_of_element_located(BUTTON_ADD_NEW_COUNTRY)).click()
         total_link = len(driver.find_element(*ELEMENT_FORM_POST).find_elements(*LINK_ON_NEW_PAGE))
         current_handle = driver.current_window_handle
         for i in range(total_link):
